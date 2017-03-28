149c608a7d3fdfd966e2e205991ade6c289c53e1
drumnation <davidmieloch@gmail.com>
2017-03-28 15:11:44 -0400

    changed class names and created class files

---
+++ app/models/lab.rb
@@ -0,0 +1,6 @@
+require_relative '../config/environment'
+
+class Lab < ActiveRecord::Base
+
+
+end
---
+++ app/models/solution.rb
@@ -0,0 +1,5 @@
+require_relative '../config/environment'
+
+class Solution < ActiveRecord::Base
+
+end
