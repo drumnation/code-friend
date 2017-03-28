87268ac6b0da2bc497d6c8ac873f0697e197442b
drumnation <davidmieloch@gmail.com>
2017-03-28 15:46:55 -0400

    fixed gitignore and db name

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,12 @@ require 'rest-client'
 Bundler.require
 
 DB = {
-  conn: SQLite3::Database.new('db/FOOBAR.sqlite')
+  conn: SQLite3::Database.new('db/code_friend.sqlite')
 }
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
-  database: 'db/FOOBAR.sqlite'
+  database: 'db/code_friend.sqlite'
 )
 
 require_all 'lib'
